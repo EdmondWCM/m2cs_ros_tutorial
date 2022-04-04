b8a4836c24f20f793c468da05fb3021256b4fe55
EdmondWCM <wongchakman1308@gmail.com>
2022-04-04 15:32:54 +0000

    update

---
+++ motor_driver/build/catkin_generated/ordered_paths.cmake
@@ -0,0 +1,1 @@
+set(ORDERED_PATHS "/home/edmond/catkin_ws/devel/lib;/opt/ros/noetic/lib")
---
+++ motor_driver/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "motor_driver"
+PROJECT_SPACE_DIR = "/home/edmond/catkin_ws/src/m2cs_ros_tutorial/motor_driver/build/devel"
+PROJECT_VERSION = "0.0.0"
